45f689cfe8f3d4798191ae18b63ba69a14ddeee8
IanOdria <ian.odria@gmail.com>
2021-09-15 10:29:19 -0500

    taps
    counts and displays each tap

--- memory_sep2021.py
+++ memory_sep2021.py
@@ -19,7 +19,7 @@ car = path('car.gif')
 tiles = list(range(32)) * 2
 state = {'mark': None}
 hide = [True] * 64
-
+taps = 0
 
 def square(x, y):
     "Draw white square with black outline at (x, y)."
@@ -44,17 +44,25 @@ def xy(count):
     return (count % 8) * 50 - 200, (count // 8) * 50 - 200
 
 
+
 def tap(x, y):
     "Update mark and hidden tiles based on tap."
     spot = index(x, y)
     mark = state['mark']
+    global taps
+    taps +=1
+    print("taps",taps)
+
 
     if mark is None or mark == spot or tiles[mark] != tiles[spot]:
         state['mark'] = spot
+
     else:
         hide[spot] = False
         hide[mark] = False
         state['mark'] = None
+        
+
 
 
 def draw():
